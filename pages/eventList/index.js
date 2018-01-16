2e49552c6d556a03172255e4c719174b5802cb6a
Xemo Tangch <jmphper@163.com>
2018-01-16 18:17:15 +0800

    edit event list

--- pages/eventList/index.js
+++ pages/eventList/index.js
@@ -4,21 +4,48 @@ Page({
    * 页面的初始数 据
    */
   data: {
-    
+    event_list : []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.event_list = wx.getStorageSync('event_list_1');
+    if(!this.event_list){
+      wx.request({
+        url: 'http://apitest.imhaiguiapp.com/api/event/NearbyList', //仅为示例，并非真实的接口地址
+        data: {
+          uid: '28117',
+          city_id: '77',
+          page: '1',
+          size: '10',
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success: function (res) {
+          console.info('success');
+          console.log(res.data);        
+          this.event_list = res.data;
+          wx.setStorageSync('event_list_1', res.data);
+        },
+        fail: function (res) {
+          console.info('fail');
+        },
+        complete: function (res) {
+          console.info('complete');
+        }
+      });
+    }
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    console.info(this.event_list);
   },
 
   /**
